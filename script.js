a7d2376f1c82a6a9c1feb628628e74aa0394b00d
njroma <njromaprofessional@gmail.com>
2023-12-11 02:06:42 +0800

    Added Bootstrap for Presentation

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 
+//1. Adding a task:
 document.querySelector("#addTask").addEventListener("click",function(){
-document.querySelector("#taskList").innerHTML += "<li>" + document.querySelector("#todoInput").value + "<button id='X'>X</button></li>";
+
+
+//5. Bonus: Input validation:
+if(document.querySelector("#todoInput").value == null || document.querySelector("#todoInput").value == ""){
+    alert("Type something First");
+}
+//1. Adding a task:
+else{
+    document.querySelector("#taskList").innerHTML += `<li class="px-1">` + document.querySelector("#todoInput").value + `<button class="bg-primary text-white m-1">X</button><br></li>`;
+}
+document.querySelector("#todoInput").value = null;
 });
 
+
+//2. Marking a task as done:
 document.querySelector("#taskList").addEventListener("click",function(selectedLi){
 if(selectedLi.target.localName == "li"){
     selectedLi.target.classList.toggle('done');
 }
+//3. Deleting a task:
 if(selectedLi.target.localName == "button"){
     selectedLi.target.parentElement.remove();
 }
 console.log(selectedLi);
 });
+
+
 
